069080cc55a416de774e4192c44972197762978b
isaacoog1 <oguntuyokoredeshile@gmail.com>
2024-01-22 07:56:59 +0100

    Initial commit

---
+++ request.js
@@ -0,0 +1,50 @@
+// const { response } = require("express");
+
+var fullname = document.getElementById("fullname");
+var email = document.getElementById("email");
+var rating = document.getElementById("rating");
+var message = document.getElementById("message");
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+document
+  .getElementById("feedback-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const dataToSend = {
+      fullname: fullname.value,
+      email: email.value,
+      rating: rating.value,
+      message: message.value,
+    };
+
+    fetch("https://feedback-form-nu.vercel.app/api/store-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        if (data === "Data saved successfully") {
+          document.getElementById("feedback-form").reset();
+          modal.style.display = "block";
+        }
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error storing data:", error);
+      });
+  });
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
