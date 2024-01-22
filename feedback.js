52d487b37156c738685505dd92973179aa29c3b3
isaacoog1 <oguntuyokoredeshile@gmail.com>
2024-01-22 08:49:21 +0100

    Added styles to the feedback page

--- feedback.js
+++ feedback.js
@@ -9,10 +9,25 @@ document.addEventListener('DOMContentLoaded', function() {
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
-        feedbacks.innerHTML = data;
+        var output = '';
+        data.forEach(feedback => {
+            output += `
+                    <div class="card my-3 w-75">
+                        <div class="card-body text-center">
+                        ${feedback.message}
+                        <div class="text-secondary mt2">
+                            By ${feedback.fullname} rated ${feedback.rating}
+                        </div>
+                        </div>
+                    </div> 
+                        `
+        })
+        feedbacks.innerHTML = output;
     })
     .catch((error) => {
         console.error("Error fetching data: ", error)
     })
 
 })
+
+    
