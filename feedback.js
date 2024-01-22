c587aef97ad7f2c7e91d101f95ecc255ed2ca599
isaacoog1 <oguntuyokoredeshile@gmail.com>
2024-01-22 17:16:18 +0100

    Added comments and updated animation logic

--- feedback.js
+++ feedback.js
@@ -1,4 +1,6 @@
 var feedbacks = document.querySelector('.feedbacks')
+
+//Make a GET request to backend when page loads
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://feedback-form-nu.vercel.app/api/get-data', {
         method: "GET",
@@ -8,8 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .then((res) => res.json())
     .then((data) => {
+        //Initialize delay for animation
         var delay = 0;
         var output = '<h2 class="lead mt3 fs-1" data-aos="zoom-in">Past Feedbacks</h2>';
+        //Loop through the data received from backend and make a template for each feedback
         data.length > 0 ? data.forEach(feedback => {
             output += `
                     <div class="card my-3 w-75" data-aos="fade-up" data-aos-delay="${delay}">
@@ -23,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         `
                         delay += 300;
         }) : output += `<p class="lead mt3">There is no feedback</p>`
+        //Append feedbacks to the container on the page
         feedbacks.innerHTML = output;
     })
     .catch((error) => {
--- request.js
+++ request.js
@@ -1,17 +1,18 @@
-// const { response } = require("express");
-
+//Select all input fields in the form
 var fullname = document.getElementById("fullname");
 var email = document.getElementById("email");
 var rating = document.getElementById("rating");
 var message = document.getElementById("message");
 
+//Select the modal
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
+//Listen for a submit event on the feedback form and make a POST request to the backend
 document
   .getElementById("feedback-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
+  .addEventListener("submit", function () {
+    //Get the current values of the input field and make an object template
     const dataToSend = {
       fullname: fullname.value,
       email: email.value,
@@ -32,24 +33,31 @@ document
           document.getElementById("feedback-form").reset();
           modal.style.display = "block";
         }
-        console.log(data);
       })
       .catch((error) => {
         console.error("Error storing data:", error);
       });
   });
 
+//Close the modal on close button click
 span.onclick = function () {
   modal.style.display = "none";
-  window.location.href = 'feedback.html';
+  window.location.href = "feedback.html";
 };
 
+//Close the modal when any paer of the window is clicked while the modal is open
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    window.location.href = 'feedback.html';
+    window.location.href = "feedback.html";
   }
 };
 
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    offset: 200,
+    duration: 1000,
+    easing: "ease-in-sine",
+  });
+  AOS.refresh();
+});
